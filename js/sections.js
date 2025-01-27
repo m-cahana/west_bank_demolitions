fa7dd703b0d5a0e7c29856596213c4d9337df50e
Michael Cahana <cahanamichael@gmail.com>
2025-01-27 11:22:19 -0500

    set up page

---
+++ js/sections.js
@@ -0,0 +1,109 @@
+"use strict";
+let svg;
+let DOT_ADJUSTMENT_FACTOR = 1;
+let ADJ_WIDTH, ADJ_HEIGHT;
+
+let CORE_MARGIN = { LEFT: 150, RIGHT: 100, TOP: 50, BOTTOM: 20 };
+let MARGIN = { LEFT: 150, RIGHT: 100, TOP: 50, BOTTOM: 20 };
+let WIDTH = 800;
+let HEIGHT = 500;
+let HEIGHT_WIDTH_RATIO = HEIGHT / WIDTH;
+
+import { scroller } from "./scroller.js";
+
+// *******************
+// read data
+// *******************
+
+d3.csv("data/raw/demolitions.csv").then((data) => {
+  console.log(data);
+
+  setTimeout(drawInitial, 100);
+});
+
+// *******************
+// functions
+// *******************
+
+function walkGenerator(totalSteps = 50, initialValue = 2, min = 0, max = 4) {
+  let v = initialValue;
+  const data = [];
+  for (let i = 0; i < totalSteps; ++i) {
+    v += Math.random() - 0.5;
+    v = Math.max(Math.min(v, max), min);
+    data.push({ step: i, value: v });
+  }
+  return data;
+}
+
+// draw each visual initially, then hide most of them
+function drawInitial() {
+  const container = document.getElementById("vis");
+  const containerWidth = container.clientWidth;
+
+  ADJ_WIDTH = Math.min(WIDTH, containerWidth);
+  ADJ_HEIGHT = ADJ_WIDTH * HEIGHT_WIDTH_RATIO;
+
+  DOT_ADJUSTMENT_FACTOR = ADJ_WIDTH / WIDTH;
+
+  MARGIN = Object.keys(CORE_MARGIN).reduce((acc, key) => {
+    acc[key] = CORE_MARGIN[key] * DOT_ADJUSTMENT_FACTOR;
+    return acc;
+  }, {});
+
+  const walk = walkGenerator();
+
+  console.log(walk);
+
+  svg = d3
+    .select("#vis")
+    .append("svg")
+    .attr("width", ADJ_WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+    .attr("height", ADJ_HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+    .attr("opacity", 1);
+
+  const walkX = d3
+    .scaleLinear()
+    .domain([0, 49])
+    .range([MARGIN.LEFT, ADJ_WIDTH - MARGIN.RIGHT]);
+
+  const walkY = d3
+    .scaleLinear()
+    .domain([0, 4])
+    .range([ADJ_HEIGHT - MARGIN.BOTTOM, MARGIN.TOP]);
+
+  const line = d3
+    .line()
+    .x((d) => walkX(d.step))
+    .y((d) => walkY(d.value));
+
+  svg.append("path").attr("d", line(walk)).attr("stroke", "black");
+}
+
+// *******************
+// scroll
+// *******************
+
+// array of all visual functions
+// to be called by the scroller functionality
+let activationFunctions = [() => {}];
+
+// scroll
+let scroll = scroller().container(d3.select("#graphic"));
+scroll();
+let lastIndex,
+  activeIndex = 0;
+scroll.on("active", function (index) {
+  activeIndex = index;
+
+  let sign = activeIndex - lastIndex < 0 ? -1 : 1;
+  let scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign);
+
+  scrolledSections.forEach((i) => {
+    activationFunctions[i]();
+  });
+  lastIndex = activeIndex;
+});
+
+// reload on top of page
+history.scrollRestoration = "manual";
